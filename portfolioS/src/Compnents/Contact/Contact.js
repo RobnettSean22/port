b7e1e849140613a52beb214041addb8c76536c10
RobnettSean22 <robnettsean22@gmail.com>
2020-01-04 17:26:41 -0800

    setting up nodemailer

--- portfolioS/src/Compnents/Contact/Contact.js
+++ portfolioS/src/Compnents/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import HeaderLink from "../HeaderLink/HeaderLink";
 import { Link } from "react-router-dom";
 import logo from "./my_logo.png";
@@ -12,105 +12,136 @@ import email from "./mail.png";
 import down from "./Down.png";
 import up from "./Up.png";
 import "./Contact.scss";
-const Contact = () => {
-  return (
-    <div className="sec">
-      <div className="cover">
-        <div className="logo-place">
-          <img className="lo-gos" src={logo} alt="tl" />
-          <HeaderLink />
-        </div>
-        <div className="title">
-          <h1>Get in Touch</h1>
-        </div>
-        <div className="spred">
-          <div className="contact-contain">
-            <div className="phone">
-              <img src={phone} alt="phone" />
-              <h1>+ 81 (080)-3939-3032</h1>
+class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      nameInp: "",
+      emailAdress: "",
+      messageInp: ""
+    };
+  }
+
+  render() {
+    const { nameInp, emailAdress, messageInp } = this.state;
+    return (
+      <div className="sec">
+        <div className="cover">
+          <div className="logo-place">
+            <img className="lo-gos" src={logo} alt="tl" />
+            <HeaderLink />
+          </div>
+          <div className="title">
+            <h1>Get in Touch</h1>
+          </div>
+          <div className="spred">
+            <div className="contact-contain">
+              <div className="phone">
+                <img src={phone} alt="phone" />
+                <h1>+ 81 (080)-3939-3032</h1>
+              </div>
+              <div className="line">
+                <img src={line} alt="line" />
+                <h1>Soul(ショニト2)</h1>
+              </div>
+              <div className="email">
+                <img src={email} alt="email" />
+                <h1>Robnettsean22@gmail.com</h1>
+              </div>
+              <div className="icons">
+                <a
+                  className="facebook"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/sean.robnett1"
+                >
+                  <img className="facebook" src={fb} alt="facebook" />
+                </a>
+                <a
+                  className="insta"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/auniversoullion/?hl=ja"
+                >
+                  <img className="insta" src={ig} alt="insta" />
+                </a>
+                <a
+                  className="github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/RobnettSean22"
+                >
+                  <img className="github" src={gh} alt="github" />
+                </a>
+                <a
+                  className="linkedin"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/sean-robnett-softwareengineer/"
+                >
+                  <img className="linkedin" src={ln} alt="linkedin" />
+                </a>
+              </div>
             </div>
-            <div className="line">
-              <img src={line} alt="line" />
-              <h1>Soul(ショニト2)</h1>
+            <div className="or">
+              <h1 className="option">OR</h1>
             </div>
-            <div className="email">
-              <img src={email} alt="email" />
-              <h1>Robnettsean22@gmail.com</h1>
-            </div>
-            <div className="icons">
-              <a
-                className="facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.facebook.com/sean.robnett1"
-              >
-                <img className="facebook" src={fb} alt="facebook" />
-              </a>
-              <a
-                className="insta"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.instagram.com/auniversoullion/?hl=ja"
-              >
-                <img className="insta" src={ig} alt="insta" />
-              </a>
-              <a
-                className="github"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://github.com/RobnettSean22"
-              >
-                <img className="github" src={gh} alt="github" />
-              </a>
-              <a
-                className="linkedin"
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.linkedin.com/in/sean-robnett-softwareengineer/"
-              >
-                <img className="linkedin" src={ln} alt="linkedin" />
-              </a>
+
+            <div className="message-contain">
+              <form>
+                <div className="name">
+                  <input
+                    value={nameInp}
+                    onChange={e => this.setState({ nameInp: e.target.value })}
+                    placeholder="Name"
+                  />
+                </div>
+                <div className="email-me">
+                  <input
+                    value={emailAdress}
+                    onChange={e =>
+                      this.setState({ emailAdress: e.target.value })
+                    }
+                    placeholder="Email"
+                  />
+                </div>
+
+                <div className="message">
+                  <textarea
+                    value={messageInp}
+                    onChange={e =>
+                      this.setState({ messageInp: e.target.value })
+                    }
+                    placeholder="Send me a Message :)"
+                  />
+                </div>
+                <div className="butbut">
+                  <button>Submit</button>
+                </div>
+              </form>
             </div>
           </div>
-          <div className="or">
-            <h1 className="option">OR</h1>
-          </div>
 
-          <div className="message-contain">
-            <form>
-              <div className="name">
-                <input placeholder="Name" />
-              </div>
-              <div className="email-me">
-                <input placeholder="Email" />
-              </div>
-
-              <div className="message">
-                <textarea placeholder="Send me a Message :)" />
-              </div>
-            </form>
+          <div className="butter">
+            <Link to={"/about/"}>
+              <img src={down} alt="down" />
+            </Link>
+            <Link to={"/resume/"}>
+              <img src={up} alt="down" />
+            </Link>
           </div>
         </div>
-
-        <div className="butter">
+        <div className="spine">
           <Link to={"/about/"}>
             <img src={down} alt="down" />
           </Link>
           <Link to={"/resume/"}>
-            <img src={up} alt="down" />
+            <img className="upC" src={up} alt="up" />
           </Link>
         </div>
       </div>
-      <div className="spine">
-        <Link to={"/about/"}>
-          <img src={down} alt="down" />
-        </Link>
-        <Link to={"/resume/"}>
-          <img className="upC" src={up} alt="up" />
-        </Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Contact;
