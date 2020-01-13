c376075a273b52cd47d1764f9ba5b522357308e8
RobnettSean22 <robnettsean22@gmail.com>
2020-01-13 19:42:20 +0900

    created build folder

---
+++ portfolioS/.netlify/build/precache-manifest.2de76daa61ab4e2ba1a49c40102e31c3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef124cda89263591ebbf572e9e0cf598",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87eb650eb461a1955249",
+    "url": "/static/css/main.2c3704d2.chunk.css"
+  },
+  {
+    "revision": "7cdaad766c4b9081ee99",
+    "url": "/static/js/2.cc7a6bab.chunk.js"
+  },
+  {
+    "revision": "87eb650eb461a1955249",
+    "url": "/static/js/main.efb0cf66.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "ff7a6ebabdd25a45dca6f16029135c0f",
+    "url": "/static/media/Got_You_web.ff7a6eba.jpg"
+  },
+  {
+    "revision": "9125989e9ac5ae54c149503c9426cfaf",
+    "url": "/static/media/KanjiNme-home.9125989e.png"
+  },
+  {
+    "revision": "2ed787e87f9c732ecf070f8eb107a054",
+    "url": "/static/media/LandName@2x.2ed787e8.png"
+  },
+  {
+    "revision": "4dfcc7dc4a83867a0aa94f12ba5393ae",
+    "url": "/static/media/js_timer.4dfcc7dc.png"
+  },
+  {
+    "revision": "ef7ba92507346cd36657539fa7de8b0d",
+    "url": "/static/media/my_logo.ef7ba925.png"
+  },
+  {
+    "revision": "e42492b24b4b69cb01323e20a02dd999",
+    "url": "/static/media/skills.e42492b2.png"
+  },
+  {
+    "revision": "ee4a0755314a96726c3abf82b7cb82bf",
+    "url": "/static/media/socketio_chat.ee4a0755.png"
+  },
+  {
+    "revision": "4ae4dca423665e62023e0e35852ea02d",
+    "url": "/static/media/song_star.4ae4dca4.png"
+  },
+  {
+    "revision": "6d9c67e18b4e90e10a3b7d7a3fd6bec9",
+    "url": "/static/media/this_is_dev.6d9c67e1.png"
+  }
+]);
